fd83bf879be2463b0d8daf4ff843c888f1857e80
dorian31 <dorian.hebert31@gmail.com>
2017-01-16 15:53:59 +0000

    feature/service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116145054) do
+ActiveRecord::Schema.define(version: 20170116144429) do
 
   create_table "service_types", force: :cascade do |t|
     t.string   "name"
