36b31f6a5950708a29fec5b31a4f1d116a8d8971
Pierre Rossignol <pierre_rossignol@yahoo.fr>
2017-01-18 10:24:28 +0000

    fixedMigration

--- db/migrate/20161214142853_add_details_to_service.rb
+++ db/migrate/20161214142853_add_details_to_service.rb
@@ -1,6 +1,6 @@
 class AddDetailsToService < ActiveRecord::Migration
   def change
-    add_column :services, :participants, :number
+    add_column :services, :participants, :integer
     add_column :services, :confirmation, :boolean
     add_column :services, :event_at, :datetime
   end
