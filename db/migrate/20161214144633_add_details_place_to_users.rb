d7560350f7d9689c2b51dde0a35f88d16e77ad79
Pierre Rossignol <pierre_rossignol@yahoo.fr>
2017-01-18 10:29:17 +0000

    fixedMigration

--- db/migrate/20161214144633_add_details_place_to_users.rb
+++ db/migrate/20161214144633_add_details_place_to_users.rb
@@ -5,7 +5,7 @@ class AddDetailsPlaceToUsers < ActiveRecord::Migration
     add_column :users, :address, :string
     add_column :users, :city, :string
     add_column :users, :postal_code, :string
-    add_column :users, :age, :number
+    add_column :users, :age, :integer
     add_column :users, :job, :string
     add_column :users, :skills, :string
     add_column :users, :rating, :string
