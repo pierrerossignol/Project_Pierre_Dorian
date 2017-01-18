45d8cbcdd65956cde6eadbeba93cee57b286014a
Pierre Rossignol <pierre_rossignol@yahoo.fr>
2017-01-18 13:50:41 +0000

    FIXED

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 def age
-   # Difference in years, less one if you have not had a birthday this year.
-    ((Date.current - birthdate) / 365).to_i
+  # Difference in years, less one if you have not had a birthday this year.
+    ((Date.current.to_i - birthdate.to_i) / 36).to_i
 end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :skills
